ee0089da75a472e703866d89e3c7c67d7f9d0847
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2018-03-19 09:53:04 -0400

    angualar app page view tracking using gtm

--- hello.js
+++ hello.js
@@ -5,10 +5,10 @@ myApp.config(function($stateProvider) {
   var states = [
     { name: 'hello', url: '/hello', component: 'hello' },
     { name: 'about', url: '/about', component: 'about' },
-    
-    { 
-      name: 'people', 
-      url: '/people', 
+
+    {
+      name: 'people',
+      url: '/people',
       component: 'people',
       resolve: {
         people: function(PeopleService) {
@@ -16,21 +16,21 @@ myApp.config(function($stateProvider) {
         }
       }
     },
-    
-    { 
-      name: 'people.person', 
-      url: '/{personId}', 
+
+    {
+      name: 'people.person',
+      url: '/{personId}',
       component: 'person',
       resolve: {
         person: function(people, $stateParams) {
-          return people.find(function(person) { 
+          return people.find(function(person) {
             return person.id === $stateParams.personId;
           });
         }
       }
     }
   ]
-  
+
   // Loop over the state definitions and register them
   states.forEach(function(state) {
     $stateProvider.state(state);
@@ -38,8 +38,23 @@ myApp.config(function($stateProvider) {
 });
 
 
-myApp.run(function($http, $uiRouter) {
+myApp.run(function($http, $uiRouter, $transitions, $location) {
   var Visualizer = window['ui-router-visualizer'].Visualizer;
   $uiRouter.plugin(Visualizer);
   $http.get('data/people.json', { cache: true });
+
+  // listen for successful transitions..
+  $transitions.onSuccess({}, function (transition) {
+      console.log('state was sucessfully changed.. ' + transition.to());
+
+      // console.log('$location.path', $location.path);
+
+      //push the event to datalayer.
+      window.dataLayer.push({
+          event: 'pageViewees',
+          action: $location.path(),
+      });
+  });
+
+
 });
